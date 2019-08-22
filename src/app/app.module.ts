2a1f4e2065d0dbead45246bc511a7a60f65ceb7c
Thierry Legros <thierry.legros2@club.fr>
2019-08-22 03:18:36 +0200

    configuration du service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { InputButtonUnitsComponent } from './input-button-units/input-button-units.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { ListManagerComponent } from './list-manager/list-manager.component';
+import { TodoListService } from './services/todo-list.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ListManagerComponent } from './list-manager/list-manager.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TodoListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
+  private todoList: TodoItem[] = [
+    {title: 'Install NodeJS'},
+    {title: 'Install Angular CLI'},
+    {title: 'Create new app'},
+    {title: 'Serve App'},
+    {title: 'Develop app'},
+    {title: 'Deploy app'},
+  ];
 
   constructor() { }
+
+  getTodoList(){
+    return this.todoList;
+  }
 }
